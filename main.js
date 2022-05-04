0768857a5eb9a20d2095043fd2828ecc2b2960f6
shloaky <77309360+shloaky@users.noreply.github.com>
2022-05-04 19:51:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+ function preload() {
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+
+function setup(){
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+}
